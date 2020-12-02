10567bb539f65fe68b089784c973d9ca983e53e7
nickb <nickblackburn02@gmail.com>
2020-12-02 12:55:40 -0800

    added toast control

--- src/main/java/space/nicholasblackburn/toastmod/events/PlayerEvents.java
+++ src/main/java/space/nicholasblackburn/toastmod/events/PlayerEvents.java
@@ -5,18 +5,43 @@
 
 package space.nicholasblackburn.toastmod.events;
 
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.gui.toasts.GuiToast;
+import net.minecraft.client.gui.toasts.IToast;
+import net.minecraft.client.gui.toasts.SystemToast;
+import net.minecraft.client.gui.toasts.TutorialToast;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
 import space.nicholasblackburn.toastmod.Main;
-
+import org.lwjgl.glfw.GLFW;
 @Mod.EventBusSubscriber
 public class PlayerEvents{
 
+    static Minecraft mc = Minecraft.getMinecraft();
+    static ScaledResolution res = new ScaledResolution(mc);
+
+    //Toast control
+    private static  GuiToast gui = new GuiToast(mc);
+    private static TutorialToast test_toset = new TutorialToast(TutorialToast.Icons.TREE, ITextComponent.Serializer.fromJsonLenient("hello"), ITextComponent.Serializer.fromJsonLenient("test"), false);
+
     @SubscribeEvent
     public static void pickupItem(EntityItemPickupEvent event) {
+
         Main.logger.info("Item picked up!"+event.getEntityPlayer().getName());
+       gui.add(test_toset);
+       gui.drawToast(res);
     }
+
+
+    @SubscribeEvent
+    public static void playerJoin(PlayerInteractEvent event) {
+        Main.logger.info("Item picked up!"+event.getEntityPlayer().getName());
+
+    }
+
+
 }
